276435a69cc3f7b1d33acb0062bc50bf3e35f13d
conner knight <connerk@uw.edu>
2018-12-21 10:55:47 -0600

    triangle

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -1,6 +1,24 @@
 ﻿namespace Shapes
 {
+   
+
+   /*
+   represents a triangle with a given Base and Height
+    */
     public class Triangle
     {
+        public int Base;
+        public int Height;
+
+        public Triangle(int baset, int height){
+            Base = baset;
+            Height = height;
+        }
+
+        public double Area(){
+            return Base * Height * (0.5);
+        }
+
+
     }
 }
