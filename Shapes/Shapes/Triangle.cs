e664f58aa3e94a8c1178a097a406a816d52a428c
justinzsy <justinzsy@gmail.com>
2018-12-21 10:55:59 -0600

    created some members and functions for Triangle.cs

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -2,5 +2,32 @@
 {
     public class Triangle
     {
+        double height;
+        double length;
+
+        double CalculateArea(double h, double l)
+        {
+            return (h * l * 0.5);
+        }
+
+        bool IsIsoscelesTriangle(double s1, double s2, double s3)
+        {
+            if (s1 == s2)
+                return true;
+            else if (s1 == s3)
+                return true;
+            else if (s2 == s3)
+                return true;
+            else
+                return false;
+        }
+
+        bool IsEquilateralTriangle(double s1, double s2, double s3)
+        {
+            if (s1 == s2 && s2 == s3)
+                return true;
+            else
+                return false;
+        }
     }
 }
