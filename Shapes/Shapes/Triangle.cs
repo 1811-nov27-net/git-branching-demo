4cfbcf9c3ef4a28c52c77f9701477b89326db427
devin-rouse <devin.a.rouse@gmail.com>
2018-12-21 10:55:47 -0600

    Add properties and methods to Triangle class

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -2,5 +2,32 @@
 {
     public class Triangle
     {
+        // Properties to measure lengths of all sides
+        public decimal Side1 { get; set; }
+        public decimal Side2 { get; set; }
+        public decimal Side3 { get; set; }
+
+        public decimal Hypotenuse { get; set; } => GetHypotenuse(Side1, Side2, Side3);
+
+        // Check that the triangle obeys the rule of "the sum of the lengths of two sides must be greater than the length of the third side"; return 'true' if rule is obeyed, 'false' if not
+        public bool IsGoodTriangle()
+        {
+            if (Side1 + Side2 < Side3 || Side1 + Side3 < Side2 || Side2 + Side3 < Side1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public decimal GetHypotenuse(decimal sideA, decimal sideB, decimal sideC)
+        {
+
+        }
+
+
+
     }
 }
