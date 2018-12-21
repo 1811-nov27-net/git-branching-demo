fbe0901ae37b32aa321354190000d975a8ce141c
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-21 10:51:54 -0600

    implement triangle

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -1,6 +1,31 @@
-﻿namespace Shapes
+﻿using System;
+
+namespace Shapes
 {
     public class Triangle
     {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangle(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public int Sides => 3;
+
+        public double Area
+        {
+            get
+            {
+                var s = (A + B + C) / 2;
+                return Math.Sqrt(s * (s - A) * (s - B) * (s - C));
+            }
+        }
+
+        public double Perimeter => A + B + C;
     }
 }
