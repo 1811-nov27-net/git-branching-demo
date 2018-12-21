dd2a4a5f3291332ada6c4c290dd7cf2306580b8d
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-12-21 10:55:22 -0600

    implement triangle another way

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -1,6 +1,24 @@
-﻿namespace Shapes
+﻿using System;
+
+namespace Shapes
 {
     public class Triangle
     {
+        public double Ax { get; set; }
+        public double Ay { get; set; }
+        public double Bx { get; set; }
+        public double By { get; set; }
+        public double Cx { get; set; }
+        public double Cy { get; set; }
+
+        public double Perimeter
+        {
+            get
+            {
+                return Math.Sqrt((By - Ay) * (By - Ay) + (Bx - Ax) * (Bx - Ax)) +
+                    Math.Sqrt((Cy - By) * (Cy - By) + (Cx - Bx) * (Cx - Bx)) +
+                    Math.Sqrt((Ay - Cy) * (Ay - Cy) + (Ax - Cx) * (Ax - Cx));
+            }
+        }
     }
 }
