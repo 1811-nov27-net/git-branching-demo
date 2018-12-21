ddf94dca95de1d3a083dba58c28a309789f39dd4
Beau Hansen <beau.hansen45@gmail.com>
2018-12-21 10:58:06 -0600

    added triangle props

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -2,5 +2,30 @@
 {
     public class Triangle
     {
+        public struct coords
+        {
+            public int x, y;
+
+            public coords(int p1, int p2)
+            {
+                x = p1;
+                y = p2;
+            }
+        }
+
+        public coords[] Trianglecoords { get; set;}
+
+        public int MyProperty { get; set; }
+
+        public double height { get; set; }
+
+        public double base1 { get; set; }
+
+        public double area
+        {
+            get => 0.5 * (height * base1);
+        }
+
+
     }
 }
