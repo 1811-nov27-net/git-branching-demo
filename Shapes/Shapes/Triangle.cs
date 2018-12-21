54185435f8cab227c38b4577ae57f3a6bcd8bc5a
Matthew Liner <mattliner028@gmail.com>
2018-12-21 10:56:00 -0600

    my triangle

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -2,5 +2,18 @@
 {
     public class Triangle
     {
+        public double sideA { set; get; }
+        public double sideB { set; get; }
+        public double sideC { set; get; }
+        public double perimeter { get; set; }
+
+        public void SetSides(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+            this.perimeter = sideA + sideB + sideC;
+
+        }
     }
 }
