21c693d81dc0bb7dc841ab7633409a5f97b6f677
willbelt <willbeltit@gmail.com>
2018-12-21 10:56:11 -0600

    wrote triangle class properties and methods

--- Shapes/Shapes/Triangle.cs
+++ Shapes/Shapes/Triangle.cs
@@ -1,6 +1,34 @@
-﻿namespace Shapes
+﻿using System.Collections.Generic;
+
+namespace Shapes
 {
     public class Triangle
     {
+        public IList<int> Sides { get; set; }
+        public IList<int> Angles { get; set; }
+        public int perimeter { get; set; }
+        public double area { get; set; }
+
+        public Triangle(IList<int> sides, IList<int> angles)
+        {
+            Sides = sides;
+            Angles = angles;
+            CalculatePerimeter();
+        }
+
+        public void CalculatePerimeter()
+        {
+            perimeter = 0;
+            foreach (var side in Sides)
+            {
+                perimeter += side;
+            }
+        }
+
+        public void CalculateArea()
+        {
+
+        }
+
     }
 }
